4453d8fd8bedb4316b2837d6e110cfa449bb457d
Otavio <otavioazevedo@live.com>
2017-05-27 22:47:53 -0300

    Otimização do gerador de exercícios e inserção de 8 novas frações

--- app/src/main/java/ubiquitouscomputing/pat2math_app/controller/Generator.java
+++ app/src/main/java/ubiquitouscomputing/pat2math_app/controller/Generator.java
@@ -2,19 +2,45 @@ package ubiquitouscomputing.pat2math_app.controller;
 
 import ubiquitouscomputing.pat2math_app.model.Exercise;
 
+import java.util.LinkedList;
+
 public class Generator {
-	private static String[][] fractions = {{"5/10", "2/8", "4/6", "6/14", "15/20"},
-										   {"10/30", "9/36", "50/100", "22/88", "4/10"},
-										   {"25/75", "77/88", "55/245", "21/36", "64/56"},
-										   {"144/156", "121/55", "55/245", "250/75", "63/119"},
-										   {"832/117", "756/81", "216/72", "954/1260", "9999/99999"}};
-	//0 = Muito Fï¿½cil
-	//1 = Fï¿½cil
-	//2 = Mï¿½dio
-	//3 = Difï¿½cil
+	private static String[][] fractions = {{"5/10", "2/8", "4/6", "6/14", "15/20", "10/30", "4/16"},
+										   {"10/30", "9/36", "50/100", "22/88", "4/10", "4/50", "60/90"},
+										   {"25/75", "77/88", "55/245", "21/36", "64/56", "98/116", "222/828"},
+										   {"144/156", "121/55", "55/245", "250/75", "63/119", "483/1932", "187/1870"},
+										   {"832/117", "756/81", "216/72", "954/1260", "9999/99999", "2310/16170", "3315/62985"}};
+	
+	private static LinkedList<String>[] fractionsLists;
+	
+	//0 = Muito Fácil
+	//1 = Fácil
+	//2 = Médio
+	//3 = Difícil
 	//4 = Desafio
+	
+	public static void generateLists() {
+		fractionsLists = (LinkedList<String>[]) new Object[5];
+		
+		for (int i = 0; i < fractionsLists.length; i++) {
+			fractionsLists[i] = new LinkedList<String>();
+			
+			for (int j = 0; i < fractions[i].length; i++) 
+				fractionsLists[i].add(fractions[i][j]);		
+		}
+	}
+	
+	public static void generateList(int difficultyLevel) {	
+		for (int j = 0; j < fractions[difficultyLevel].length; j++) {
+			//Inverte a fração para se tornar um exercício diferente
+			String[] temp = fractions[difficultyLevel][j].split("/");
+			fractions[difficultyLevel][j] = temp[1] + "/" + temp[0];
+			
+			fractionsLists[difficultyLevel].add(fractions[difficultyLevel][j]);
+		}
+	}
+	
 	public static Exercise generateExercise(int difficultyLevel) {
-		int pos = (int) Math.random() * fractions[difficultyLevel].length;
 		int maxPoints = 20;
 		
 		if (difficultyLevel == 1)
@@ -29,7 +55,19 @@ public class Generator {
 		else
 			maxPoints = 100;
 		
-		Fraction f = new Fraction(fractions[difficultyLevel][pos]);
+		Fraction f;
+		int sizeList = fractionsLists[difficultyLevel].size();
+		
+		//Neste momento, a lista atual ficará vazia e deverá ser preenchida novamente
+		if (sizeList == 1) {
+			f = new Fraction(fractionsLists[difficultyLevel].removeFirst());
+			generateList(difficultyLevel);
+		}
+		
+		else {
+			int pos = (int) Math.random() * sizeList;
+			f = new Fraction(fractionsLists[difficultyLevel].remove(pos));
+		}
 		
 		return new Exercise(f, maxPoints);
 	}
